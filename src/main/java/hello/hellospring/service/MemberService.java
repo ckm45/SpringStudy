df3eaf6fcc34dc07be73a136ae6489577fad5ca2
ckm45 <ckm45@naver.com>
2023-07-29 15:11:41 +0900

    설정파일을 통해 자바코드로 직접 스프링 빈 등록

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 
-@Service
+
 public class MemberService {
 
 
